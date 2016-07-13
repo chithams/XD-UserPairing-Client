c5d6267b7b43ec4f5328485f28d77fa8dec27bec
Ranjini <sranjinic@hotmail.com>
2016-07-13 23:28:59 +0200

    code clean-up

--- js/userpairing.js
+++ js/userpairing.js
@@ -1,13 +1,32 @@
-/**
- * Created by Ranjini on 21.05.2016.
+/*
+ * XD-MVC -- A framework for cross-device applications
+ * Copyright (C) 2014-2015 Maria Husmann. All rights reserved.
+ *
+ * This file was created by Sivaranjini Chithambaram.
+ *
+ * XD-MVC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * XD-MVC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with XD-MVC. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * See the README and LICENSE files for further information.
+ *
  */
-
 
 XDMVC.prototype.userSignIn = function userSignIn(userID){
     XDmvc.sendToServer('userSignIn',userID, function(deviceId){
         this.emit('signedIn', deviceId);
     }.bind(this));
 };
+
 XDMVC.prototype.userSignOut = function userSignOut(userID){
     XDmvc.sendToServer('userSignOut', userID, function(res){
         console.log(res);
@@ -22,6 +41,7 @@ XDMVC.prototype.getLocation = function  getLocation() {
         console.log("Geolocation is not suppemorted by this browser.");
     }
 };
+
 XDMVC.prototype.showPosition = function showPosition(position) {
     console.log(position);
     var latitude = position.coords.latitude;
@@ -29,9 +49,11 @@ XDMVC.prototype.showPosition = function showPosition(position) {
     console.log(latitude+' / '+longitude);
     XDmvc.logLocation(latitude,longitude);
 };
+
 XDMVC.prototype.logLocation = function logLocation(lat,lon){
     XDmvc.sendToServer('logLocation',[lat,lon]);
 };
+
 XDMVC.prototype.logDistance = function logDistance(contactID){
     XDmvc.sendToServer('logDistance',contactID,function(dist){
     });
@@ -45,6 +67,7 @@ XDMVC.prototype.enterRelationship = function enterRelationship(contactID,relatio
         }
     }.bind(this));
 };
+
 XDMVC.prototype.checkContactOnline = function checkContactOnline(contactName, contactID,relationshipName, callback){
     XDmvc.sendToServer('isContactOnline',contactID, function(data){
         if(data) {
@@ -54,6 +77,7 @@ XDMVC.prototype.checkContactOnline = function checkContactOnline(contactName, co
         }
     }.bind(this));
 };
+
 XDMVC.prototype.pairFriends = function pairFriends(contactID){
     this.sendToServer('pairfriends',["contact",contactID],function(data){
         if(data){
@@ -66,6 +90,7 @@ XDMVC.prototype.pairFriends = function pairFriends(contactID){
         }
     }.bind(this));
 };
+
 XDMVC.prototype.pairDevice = function pairDevice(deviceID,contactID){
     this.sendToServer('pairfriends',["device",deviceID,contactID],function(data){
         if(data){
@@ -93,7 +118,6 @@ XDMVC.prototype.removeDevice = function removeDevice(){
 XDMVC.prototype.getFriendsSelected = function getFriendsSelected(groupNames, callback){
 
     XDmvc.sendToServer('getFriendsSelected',groupNames,function(data){
-        // console.log(data);
         callback(data);
     }.bind(this))
 
@@ -110,23 +134,17 @@ XDMVC.prototype.getPairingRequests = function getPairingRequests(){
             var contactList = Object.keys(JSON.parse(data));
             this.emit('newPairingRequests', contactList);
         }
-        else{
-            // console.log("no pairing requests");
-        }
     }.bind(this));
 };
 
 XDMVC.prototype.configureGroups = function configureGroups(groups){
     var indexes = Object.keys(groups);
-
     var groupInfo = {};
+
     for(var i = 0; i < indexes.length; i ++){
         var obj = groups[indexes[i]];
-        console.log(obj)
         groupInfo[obj["name"]] = obj["requiresAck"];
     }
-
-    console.log(groupInfo);
 
     this.sendToServer('configGroups',groupInfo);
 };
